b58ec9b04e1691836efe70470d3f69ef83473ed5
Robert Lane <RobertLaneJr@gmail.com>
2023-04-10 04:51:17 -0500

    updated Rshiny app to have selectable logarithmic scale

--- Rshiny.R
+++ Rshiny.R
@@ -22,7 +22,13 @@ ui <- fluidPage(
     sidebarPanel(
       
       # Select the neighborhood class
-      selectInput("neighborhood", "Select Neighborhood Class", choices = c("All", unique(train_data$Neighborhood)))
+      selectInput("neighborhood", "Select Neighborhood Class", choices = c("All", unique(train_data$Neighborhood))),
+      
+      # Checkbox to display x-axis on a logarithmic scale
+      checkboxInput("log_x", "Logarithmic x-axis", value = FALSE),
+      
+      # Checkbox to display y-axis on a logarithmic scale
+      checkboxInput("log_y", "Logarithmic y-axis", value = FALSE)
       
     ),
     
@@ -52,9 +58,17 @@ server <- function(input, output) {
   
   # Create the ggplot based on the filtered data
   output$housingPlot <- renderPlot({
+    
+    # Apply logarithmic scaling to x-axis if selected
+    x_scale <- if(input$log_x) {scale_x_continuous(trans = "log")} else {NULL}
+    
+    # Apply logarithmic scaling to y-axis if selected
+    y_scale <- if(input$log_y) {scale_y_continuous(trans = "log")} else {NULL}
+    
     ggplot(filtered_data(), aes(x = GrLivArea, y = SalePrice, color = Neighborhood)) +
       geom_point() +
-      scale_color_manual(values = color_dict)
+      scale_color_manual(values = color_dict) +
+      x_scale + y_scale
   })
 }
 
